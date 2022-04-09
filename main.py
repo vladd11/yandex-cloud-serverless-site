f8d7b65c59efea7f67360c07b2f7003968146b7d
Vladd11 <rozhkov.2006@gmail.com>
2022-04-09 16:22:40 +0400

    Added CLI using Fire library

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import os
 
+from fire import Fire
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from db import Database
@@ -12,17 +13,23 @@ env = Environment(
 )
 
 template = env.get_template("index.html")
+db = Database(os.environ.get("ENDPOINT"), os.environ.get("DATABASE"))
+deployer = Deployer()
 
-if __name__ == '__main__':
-    db = Database(os.environ.get("ENDPOINT"), os.environ.get("DATABASE"))
 
-    deployer = Deployer()
+def deploy():
+    """
+    Deploy site to Yandex Cloud Object Storage
+    """
     for path in glob.glob('app\\templates\\*'):
-        base = os.path.basename(path)
-
         # Check that file doesn't have sub-extensions. It's need to prevent uploading of base template files
+        base = os.path.basename(path)
         if os.path.splitext(os.path.splitext(base)[0])[1] == '':
             deployer.add_page(base, template.render(products=db.get_products()))
             print(f'Deployed: {base}')
 
-    db.disconnect()
+
+if __name__ == '__main__':
+    Fire({"deploy": deploy}, name="Yandex Cloud serverless site")
+
+db.disconnect()
