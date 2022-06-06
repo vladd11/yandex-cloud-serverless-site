16aeac64c4964cb09af582ca936a0d0d1cc103f6
vladd11 <rozhkov.2006@gmail.com>
2022-06-06 11:28:29 +0400

    Removed tests, added dev server.

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const index = require('./index')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(require('cors')())
+app.use(bodyParser.text())
+
+const port = 3000
+
+app.post('/', async (req, res) => {
+    const result = await index.handler({
+        isBase64Encoded: false,
+        body: req.body,
+        requestContext: {
+            identity: {
+                sourceIp: "127.0.0.1",
+                userAgent: req.headers["user-agent"]
+            }
+        }
+    });
+    res.send(result.body)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
--- test/config.js
+++
@@ -1,8 +0,0 @@
-module.exports.requestContext = {
-    identity: {
-        sourceIp: "0.0.0.0",
-        userAgent: "MockTest"
-    }
-};
-
-module.exports.jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImE1MDJhNzVhNzZjNjI3Mjc1OThlYzRiMmVmODk2MWYxIiwicGhvbmUiOiIrNzEyMzQ1Njc4OTAiLCJpYXQiOjE2NTQzMzY2MDF9.bzozI4iggxk1ij3idPWICqNAq9COiVfJH2PIJoWynNE"
